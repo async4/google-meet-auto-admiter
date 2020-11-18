3fbcdb610eb89f8199e58141bc983728c16e7fed
sergen cepoglu <async.src.4@gmail.com>
2020-11-18 11:52:49 +0300

    🥳 Done

---
+++ admiter.py
@@ -0,0 +1,82 @@
+import re
+import sys
+import time
+
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, WebDriverException, ElementNotVisibleException
+from selenium.webdriver.common.keys import Keys
+
+
+class Admiter:
+    def __init__(self, driver):
+        self.driver = driver
+        self.__connection = False
+    
+    @property
+    def connection(self):
+        return self.__connection
+    
+    @connection.setter
+    def connection(self, value):
+        self.__connection = value
+
+    def connect(self):
+        link = "https://apps.google.com/intl/en-US/meet/"
+        print("🌐 waiting {}" .format(link))
+
+        try:
+            self.driver.get(link)
+            print("✅ connection success")
+            self.connection = True
+        except:
+            print("❌ connection error")
+            self.connection = False
+
+    def parse_url(self, url):
+        regex = r"^((http|https)\:\/\/(meet\.google\.com\/)?[a-z]{0,}\-[a-z]{0,}\-[a-z]{0,})"
+        matches = re.finditer(regex, url)
+        
+        for match_num, match in enumerate(matches, start=1):
+            if match.group():
+                return True
+
+        return False
+
+    def is_admit(self):
+        admit_button = "//body/div[@id='yDmH0d']/div[3]/div[1]/div[2]/div[3]/div[3]/span[1]/span[1]"
+        try:
+            element = self.driver.find_element(By.XPATH, admit_button)
+            element.click()
+            print("🥳 Incoming request accepted")
+        except:
+            pass
+
+    def listen(self):
+        while self.connection:
+            if self.driver:
+                try:
+                    current_url = self.driver.current_url
+                    print("🟢 {}" .format(current_url))
+                    
+                    if self.parse_url(current_url):
+                        self.is_admit()
+                        pass
+
+                except ConnectionRefusedError:
+                    print("🔴 {}" .format("connection refused. reconnecting"))
+                    continue
+                except WebDriverException:
+                    print("💀 Driver shutdown. ( {} )" .format(datetime.now()))
+                    self.connection = False
+                finally:
+                    time.sleep(0.2)
+            else:
+                print("🔎 Driver not found")
+                sys.exit()
+
+
---
+++ manage.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import time
+
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+
+import admiter as adm
+
+
+base_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'admiter')
+driver_url = os.path.join(base_dir, 'driver')
+
+driver_win32_url = 'win32\{version}\chromedriver.exe'
+driver_darwin_url = 'darwin/{version}/chromedriver'
+driver_linux_url = 'linux/{version}/chromedriver'
+
+
+def info(os_name, chromedriver_path):
+    print("🕕 Time: {}\n💻 OS: {}\n🔥 Chrome Driver: {}\n" .format(datetime.now(), os_name, chromedriver_path))
+
+def run(chromedriver_path):
+    with webdriver.Chrome(executable_path=chromedriver_path) as driver:
+        admiter = adm.Admiter(driver)
+        admiter.connect()
+        admiter.listen()
+
+def main():
+    os_name = sys.platform
+    chromedriver_path = None
+
+    if os_name == "win32":
+        chromedriver_path = os.path.join(driver_url, driver_win32_url.format(version='86'))
+    elif os_name == "darwin":
+        chromedriver_path = os.path.join(driver_url, driver_darwin_url.format(version='86'))
+    else:
+        chromedriver_path = os.path.join(driver_url, driver_linux_url.format(version='86'))
+    
+    info(os_name, chromedriver_path)
+    run(chromedriver_path)
+    
+    
+if __name__ == '__main__':
+    main()
