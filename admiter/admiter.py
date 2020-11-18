6057f5e433fd1921fd799c48f022b435d5b4f6a8
sergen☕️ <53083523+async4@users.noreply.github.com>
2020-11-18 13:07:08 +0300

    Update admiter.py

--- admiter/admiter.py
+++ admiter/admiter.py
@@ -8,8 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, WebDriverException, ElementNotVisibleException
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException, WebDriverException
 
 
 class Admiter:
